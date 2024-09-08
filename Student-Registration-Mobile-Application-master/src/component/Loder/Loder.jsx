d2b5a6b390032c9f76faaa8c12ab104c9c91eb77
yehan@2000 <heshanishehara510@gmail.com>
2024-09-08 12:57:05 +0530

    update

--- Student-Registration-Mobile-Application-master/src/component/Loder/Loder.jsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import {ActivityIndicator, StyleSheet} from 'react-native';
-
-export default function Loder() {
-  return (
-  <div>Loading...</div>
-  )
-}
